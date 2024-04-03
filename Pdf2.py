7e01a599a55d919e7f094397f8bcb3be05e477bd
NiladriMullick <39458493+NiladriMullick@users.noreply.github.com>
2024-04-03 13:22:15 +0530

    Update Pdf2.py

--- Pdf2.py
+++ Pdf2.py
@@ -1,4 +1,4 @@
-from pdfrw import PdfReader, PdfWriter, PageObject, PdfDict
+from pdfrw import PdfReader, PdfWriter, PageObject
 
 def highlight_word(input_pdf_path, output_pdf_path, word_to_highlight):
     # Open the input PDF
@@ -9,7 +9,7 @@ def highlight_word(input_pdf_path, output_pdf_path, word_to_highlight):
     for page in input_pdf.pages:
         # Extract text from the page
         text = page.extract_text()
-        
+
         # Check if the word is present in the text
         if word_to_highlight in text:
             # Find the position of the word
@@ -17,14 +17,17 @@ def highlight_word(input_pdf_path, output_pdf_path, word_to_highlight):
             end_index = start_index + len(word_to_highlight)
 
             # Get the coordinates of the word on the page
-            x0, y0, x1, y1 = page.MediaBox[0], page.MediaBox[1], page.MediaBox[2], page.MediaBox[3]
+            word_x0, word_y0, word_x1, word_y1 = page.MediaBox[0], page.MediaBox[1], page.MediaBox[2], page.MediaBox[3]
 
             # Add highlight annotation to the word's position
-            annot = PageObject.create_annotation('Highlight',
-                                                 Rect=[x0, y0, x1, y1],
-                                                 Contents='',
-                                                 QuadPoints=[x0, y0, x1, y0, x0, y1, x1, y1])
-            page[Annots] = page.get('/Annots', []) + [annot]
+            highlight_annot = PageObject.create_annotation('Highlight',
+                                                           Rect=[word_x0, word_y0, word_x1, word_y1],
+                                                           Contents='',
+                                                           Name='HL')
+            if '/Annots' in page:
+                page.Annots.append(highlight_annot)
+            else:
+                page.Annots = [highlight_annot]
 
         # Add the modified page to the output PDF
         output_pdf.addpage(page)
